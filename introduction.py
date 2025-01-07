56cc0329da7de1b9e47fce777a25809fef42fabc
Saad <saadjamil504@gmail.com>
2025-01-07 19:04:39 +0500

    introduction.py

---
+++ introduction.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def introduction():
+    st.title("Exploring the Heart Disease Dataset")
+
+    st.markdown("""
+    Welcome to this project! Here, we analyze the **Heart Dataset** to uncover patterns and insights.
+
+    ---
+
+    ### **Objectives**
+    - Explore dataset structure
+    - Handle missing or inconsistent data
+    - Visualize key trends
+    - Highlight actionable insights
+
+    ---
+    """)
